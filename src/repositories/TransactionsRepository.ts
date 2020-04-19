c0e6527a30a438c448564b5366dbbb743269216d
Matheus Seabra <matheusvieiracoelho@gmail.com>
2020-04-19 13:01:12 -0300

    Resolve challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionList {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -14,15 +25,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance(): TransactionList {
+    const allTransactions = this.transactions;
+
+    const initialBalance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const balance = this.transactions.reduce((acc, transaction) => {
+      if (transaction.type === 'income') {
+        return {
+          income: acc.income + transaction.value,
+          outcome: acc.outcome,
+          total: acc.total + transaction.value,
+        };
+      }
+      return {
+        income: acc.income,
+        outcome: acc.outcome + transaction.value,
+        total: acc.total - transaction.value,
+      };
+    }, initialBalance);
+
+    return { transactions: allTransactions, balance };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
